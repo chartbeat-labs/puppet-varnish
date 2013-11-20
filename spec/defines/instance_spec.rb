99adf6e14ca0e44008b77eabe3ca24e93a9fc6cd
Nate Butler <nathan.butler@gmail.com>
2013-11-20 15:52:46 -0500

    Fixup tests

--- spec/defines/instance_spec.rb
+++ spec/defines/instance_spec.rb
@@ -33,17 +33,17 @@ describe 'varnish::instance', :type => :define do
           .with_content(/-a :6081/) \
           .with_content(/-T 127.0.0.1:6082/) \
           .with_content(/-t 120/) \
-          .with_content(/-w 5, 500, 300/) \
+          .with_content(/-w 5,500,300/) \
           .with_content(%r{-S /etc/varnish/secret}) \
           .with_content(%r{-s file,/var/lib/varnish/default/varnish_storage.bin,70%}) \
           .with_content(/-p auto_restart=on/) \
           .with_content(/-p ban_dups=on/) \
           .with_content(/-p ban_lurker_sleep=0.01/) \
-          .with_content(/-p between_bytes_timeout=60/) \
+          .with_content(/-p between_bytes_timeout=60s/) \
           .with_content(/-p cli_buffer=8192/) \
-          .with_content(/-p cli_timeout=10/) \
+          .with_content(/-p cli_timeout=10s/) \
           .with_content(/-p clock_skew=10/) \
-          .with_content(/-p connect_timeout=0.7/) \
+          .with_content(/-p connect_timeout=0.7s/) \
           .with_content(/-p critbit_cooloff=180.0/) \
           .with_content(/-p default_grace=10/) \
           .with_content(/-p default_keep=0/) \
@@ -52,7 +52,7 @@ describe 'varnish::instance', :type => :define do
           .with_content(/-p expiry_sleep=1/) \
           .with_content(/-p fetch_chunksize=128/) \
           .with_content(/-p fetch_maxchunksize=262144/) \
-          .with_content(/-p first_byte_timeout=60/) \
+          .with_content(/-p first_byte_timeout=60s/) \
           .with_content(/-p gzip_level=6/) \
           .with_content(/-p gzip_memlevel=8/) \
           .with_content(/-p gzip_stack_buffer=32768/) \
@@ -73,13 +73,13 @@ describe 'varnish::instance', :type => :define do
           .with_content(/-p max_restarts=4/) \
           .with_content(/-p nuke_limit=50/) \
           .with_content(/-p ping_interval=3/) \
-          .with_content(/-p pipe_timeout=60/) \
+          .with_content(/-p pipe_timeout=60s/) \
           .with_content(/-p prefer_ipv6=off/) \
           .with_content(/-p queue_max=100/) \
           .with_content(/-p rush_exponent=3/) \
           .with_content(/-p saintmode_threshold=10/) \
-          .with_content(/-p send_timeout=60/) \
-          .with_content(/-p sess_timeout=5/) \
+          .with_content(/-p send_timeout=60s/) \
+          .with_content(/-p sess_timeout=5s/) \
           .with_content(/-p sess_workspace=65536/) \
           .with_content(/-p session_linger=50/) \
           .with_content(/-p session_max=100000/) \
@@ -111,9 +111,9 @@ describe 'varnish::instance', :type => :define do
           .with_content(%r{.window = 10}) \
           .with_content(%r{.threshold = 6}) \
           .with_content(%r{.expected_response = 200}) \
-          .with_content(/backend backend0.*host\ =\ "127\.0\.0\.1";.*port\ =\ "8080";.*first_byte_timeout\ =\ 60;/sm) \
+          .with_content(/backend backend0.*host\ =\ "127\.0\.0\.1";.*port\ =\ "8080";.*first_byte_timeout\ =\ 60s;/sm) \
           .with_content(/backend = backend0;/) \
-          .with_content(/acl purge \{\n\s*localhost;\n\}/m) \
+          .with_content(/acl purge \{\n\s*"localhost";\n\}/m) \
           .with_content(/include "subs-default.vcl";/)
     }
     it { should contain_file('/etc/varnish/subs-default.vcl') }
