49be7dae5f6693dd5ea1a993d62a924dcb4d6a73
Nate Butler <nathan.butler@gmail.com>
2013-11-20 22:01:40 -0500

    Add tests for varnish::instance resources

--- spec/defines/instance_spec.rb
+++ spec/defines/instance_spec.rb
@@ -12,22 +12,43 @@ describe 'varnish::instance', :type => :define do
 
   describe 'when installing a default varnish instance' do
     it { should contain_varnish__instance('default').with({
-           'ensure' => 'running'
+           :ensure => 'running',
       })
     }
-    it { should contain_file('/etc/init.d/varnish-default') \
+    it { should contain_file('/etc/init.d/varnish-default').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0700',
+          }) \
           .with_content(/^NAME=varnishd-default$/) \
           .with_content(/^ulimit -n 131072$/) \
           .with_content(/^ulimit -l 82000$/) \
           .with_content(%r{/etc/default/varnish-default})
     }
-    it { should contain_file('/etc/init.d/varnishlog-default') \
+    it { should contain_file('/etc/init.d/varnishlog-default').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0700',
+          }) \
           .with_content(/^NAME=varnishlog-default$/)
     }
-    it { should contain_file('/etc/init.d/varnishncsa-default') \
+    it { should contain_file('/etc/init.d/varnishncsa-default').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0700',
+          }) \
           .with_content(/^NAME=varnishncsa-default$/)
     }
-    it { should contain_file('/etc/default/varnish-default') \
+    it { should contain_file('/etc/default/varnish-default').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0644',
+          }) \
+          .with('ensure' => 'present') \
           .with_content(/-n default/) \
           .with_content(%r{-f /etc/varnish/main-default.vcl}) \
           .with_content(/-a :6081/) \
@@ -98,13 +119,28 @@ describe 'varnish::instance', :type => :define do
           .with_content(/-p vcc_err_unref=on/) \
           .with_content(/-p vcl_trace=off/)
     }
-    it { should contain_file('/etc/default/varnishlog-default') \
+    it { should contain_file('/etc/default/varnishlog-default').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0644',
+          }) \
           .with_content(/^# VARNISHLOG_ENABLED=1$/)
     }
-    it { should contain_file('/etc/default/varnishncsa-default') \
+    it { should contain_file('/etc/default/varnishncsa-default').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0644',
+          }) \
           .with_content(/^# VARNISHNCSA_ENABLED=1$/)
     }
-    it { should contain_file('/etc/varnish/main-default.vcl') \
+    it { should contain_file('/etc/varnish/main-default.vcl').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0644',
+          }) \
           .with_content(%r{.url = "/alive"}) \
           .with_content(%r{.timeout = 300ms}) \
           .with_content(%r{.interval = 1s}) \
@@ -116,7 +152,28 @@ describe 'varnish::instance', :type => :define do
           .with_content(/acl purge \{\n\s*"localhost";\n\}/m) \
           .with_content(/include "subs-default.vcl";/)
     }
-    it { should contain_file('/etc/varnish/subs-default.vcl') }
+    it { should contain_file('/etc/varnish/subs-default.vcl').with({
+            :ensure => 'present',
+            :owner => 'root',
+            :group => 'root',
+            :mode => '0644',
+          })
+    }
+    it { should contain_service("varnish-default").with({
+            :ensure => 'running',
+            :enable => true,
+          })
+    }
+    it { should contain_service("varnishlog-default").with({
+            :ensure => 'stopped',
+            :enable => false,
+          })
+    }
+    it { should contain_service("varnishlog-default").with({
+            :ensure => 'stopped',
+            :enable => false,
+          })
+    }
   end
 
   describe 'when uninstalling a default varnish instance' do
