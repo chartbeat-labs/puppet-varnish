f4379507daf0dadec6de2fb6f524cc386cb37276
Nate Butler <nathan.butler@gmail.com>
2013-11-23 00:27:24 -0500

    Refactor varnish::instance system tests
     * Trying to prematurely optimize with contexts led to a failure of
       serverspec system tests which apparently execute all at once at the
       END of a describe section.

--- spec/system/instance_spec.rb
+++ spec/system/instance_spec.rb
@@ -1,86 +1,124 @@
 require 'spec_helper_system'
 
-describe 'instance tests' do
-  context "with 1 instance" do
-    it 'should work with no errors and be idempotent' do
-      pp = <<-EOS
-        varnish::instance { 'default': }
-      EOS
+describe 'instance tests: 1 instance install' do
+  it 'should work with no errors and be idempotent' do
+    pp = <<-EOS
+      varnish::instance { 'default': }
+    EOS
 
-      puppet_apply(pp) do |r|
-        r.exit_code.should == 2
-        r.refresh
-        r.exit_code.should be_zero
-      end
-    end
-
-    describe service('varnish-default') do
-      it { should be_enabled }
-      it { should be_running }
-    end
-
-    it 'should safely remove itself' do
-      pp = <<-EOS
-        varnish::instance { 'default': ensure => 'purged' }
-      EOS
-
-      puppet_apply(pp) do |r|
-        r.exit_code.should == 2
-      end
+    puppet_apply(pp) do |r|
+      r.exit_code.should == 2
+      r.refresh
+      r.exit_code.should be_zero
     end
   end
-  context "with 2 instances" do
-    it 'should work with no errors and be idempotent' do
-      pp = <<-EOS
-        varnish::instance { 'inst1': }
-        varnish::instance { 'inst2':
-          address => [ ':6083' ],
-          admin_address => '127.0.0.1:6084',
-        }
-      EOS
 
-      puppet_apply(pp) do |r|
-        r.exit_code.should == 2
-        r.refresh
-        r.exit_code.should be_zero
-      end
-    end
-    it 'should safely remove itself' do
-      pp = <<-EOS
-        varnish::instance { 'inst1': ensure => 'purged' }
-        varnish::instance { 'inst2': ensure => 'purged' }
-      EOS
+  describe service('varnish-default') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
 
-      puppet_apply(pp) do |r|
-        r.exit_code.should == 2
-      end
+describe 'instance test: 1 instance removal' do
+  it 'should safely remove itself' do
+    pp = <<-EOS
+      varnish::instance { 'default': ensure => 'purged' }
+    EOS
+
+    puppet_apply(pp) do |r|
+      r.exit_code.should == 2
     end
   end
-  context "with a dependent package installed by another means" do
-    it 'should work with no errors and be idempotent' do
-      pp = <<-EOS
-        package { 'git-core':
-          ensure => 'installed',
-          before => Varnish::Instance[inst1],
-        }
-        varnish::instance { 'inst1': }
-      EOS
 
-      puppet_apply(pp) do |r|
-        r.exit_code.should == 2
-        r.refresh
-        r.exit_code.should be_zero
-      end
-    end
-    it 'should safely remove itself' do
-      pp = <<-EOF
-        package { 'git-core': ensure => 'purged' }
-        varnish::instance { 'inst1': ensure => 'purged' }
-      EOF
+  describe service('varnish-default') do
+    it { should_not be_running }
+  end
+end
 
-      puppet_apply(pp) do |r|
-        r.exit_code.should == 2
-      end
+describe 'instance test: 2 instances install' do
+  it 'should work with no errors and be idempotent' do
+    pp = <<-EOS
+      varnish::instance { 'inst1': }
+      varnish::instance { 'inst2':
+        address => [ ':6083' ],
+        admin_address => '127.0.0.1:6084',
+      }
+    EOS
+
+    puppet_apply(pp) do |r|
+      r.exit_code.should == 2
+      r.refresh
+      r.exit_code.should be_zero
     end
+  end
+
+  describe service('varnish-inst1') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service('varnish-inst2') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
+describe 'instance test: 2 instances removal' do
+  it 'should safely remove itself' do
+    pp = <<-EOS
+      varnish::instance { 'inst1': ensure => 'purged' }
+      varnish::instance { 'inst2': ensure => 'purged' }
+    EOS
+
+    puppet_apply(pp) do |r|
+      r.exit_code.should == 2
+    end
+  end
+
+  describe service('varnish-inst1') do
+    it { should_not be_running }
+  end
+
+  describe service('varnish-inst2') do
+    it { should_not be_running }
+  end
+
+end
+
+describe 'instance install: with a dependent package installed independently' do
+  it 'should work with no errors and be idempotent' do
+    pp = <<-EOS
+      package { 'git-core':
+        ensure => 'installed',
+        before => Varnish::Instance[inst1],
+      }
+      varnish::instance { 'inst1': }
+    EOS
+
+    puppet_apply(pp) do |r|
+      r.exit_code.should == 2
+      r.refresh
+      r.exit_code.should be_zero
+    end
+  end
+
+  describe package('git-core') do
+    it { should be_installed }
+  end
+end
+
+describe 'instance install: purged instance should not remove package' do
+  it 'should safely remove itself' do
+    pp = <<-EOF
+      varnish::instance { 'inst1': ensure => 'purged' }
+    EOF
+
+    puppet_apply(pp) do |r|
+      r.exit_code.should == 2
+    end
+  end
+
+  describe package('git-core') do
+    it { should be_installed }
   end
 end
