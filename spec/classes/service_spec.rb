4f5f30e51b35e417f5fdbcf6042b57b9eccbfcbf
Nate Butler <nathan.butler@gmail.com>
2015-03-05 20:30:57 -0500

    Get tests working with latest rspec-puppet, pin puppet-lint

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -4,6 +4,7 @@ describe 'varnish::service' do
   describe 'varnish::service class on Debian' do
     let(:facts) {{
       :osfamily => 'Debian',
+      :lsbdistid => 'Ubuntu',
     }}
 
     it { should contain_service('varnish').with({
